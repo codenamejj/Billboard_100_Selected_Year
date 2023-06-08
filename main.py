ff8f0cf6d4588c5a7b31b9087083e2b0814588a6
John Maroa <112829127+codenamejj@users.noreply.github.com>
2023-06-08 16:51:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
 URL = f"https://www.billboard.com/charts/hot-100/{date}"
 
 response = requests.get(url=URL)
 webs = response.text
 
 soup = BeautifulSoup(webs, "html.parser")
+
 top_song = soup.find(name="h3", class_="c-title a-font-primary-bold-l a-font-primary-bold-m@mobile-max lrv-u-color"
-                                           "-black u-color-white@mobile-max lrv-u-margin-r-150")
+                                       "-black u-color-white@mobile-max lrv-u-margin-r-150")
 
 other_songs = soup.find_all(name="h3",
                             class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size"
                                    "-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max "
                                    "a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
-
+top_song_list = []
 for top in top_song:
-    print("1: " + top.text.strip())
+    top_song_list.append(top.text.strip())
 
-count = 1
+other_songs_list = []
 for songs in other_songs:
-    count += 1
-    print(f"{count}: " + songs.text.strip())
+    other_songs_list.append(songs.text.strip())
+
+while "" in top_song_list:
+    top_song_list.remove("")
+
+song_100_list = top_song_list + other_songs_list
+print(song_100_list)
